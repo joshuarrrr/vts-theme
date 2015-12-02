e0e4ad018e34a3259d350e4712cfff9c0e23f0d6
Josh Romero <josh@joshromero.com>
2015-12-02 00:45:30 -0700

    revert debugging change

--- wp-content/themes/vtstheme/404.php
+++ wp-content/themes/vtstheme/404.php
@@ -22,7 +22,7 @@ get_header(); ?>
 
 					<?php get_search_form(); ?>
 
-					<?php __contstruct( 'WP_Widget_Recent_Posts' ); ?>
+					<?php the_widget( 'WP_Widget_Recent_Posts' ); ?>
 
 					<?php if ( vtstheme_categorized_blog() ) : // Only show the widget if site has multiple categories. ?>
 					<div class="widget widget_categories">
